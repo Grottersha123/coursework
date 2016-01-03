685c1e25734554f38d4f00ef3333d23150337035
Grottersha123 <grottersha123@gmail.com>
2016-01-03 04:00:36 +0300

    Здесь будет реализовано конвертирование все в одном TextBox пока что без изменений

--- Kursach/Form1.Designer.cs
+++ Kursach/Form1.Designer.cs
@@ -7,7 +7,10 @@ namespace Kursach
         /// Требуется переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-       
+        int a,c;
+        string b;
+        int count;
+        //bool sign;
         public static string Arabic_Parse(int  arabic)
         {
             
@@ -37,6 +40,31 @@ namespace Kursach
 
             }
             return S;
+        }
+        public void calculate()
+        {
+            switch(count)
+            {
+                case 1:
+                    c = Roman_Parse(textBox3.Text) + a;
+                    textBox3.Text = c.ToString();
+                    break;
+                case 2:
+                    c = -Roman_Parse(textBox3.Text) + a;
+                    textBox3.Text = c.ToString();
+                    break;
+                case 3:
+                    c = Roman_Parse(textBox3.Text) * a;
+                    textBox3.Text = c.ToString();
+                    break;
+                case 4:
+                    c = a / Roman_Parse(textBox3.Text);
+                    textBox3.Text = c.ToString();
+                    break;
+
+
+
+            }
         }
 
         public int Roman_Parse(string roman)
@@ -116,8 +144,9 @@ namespace Kursach
             this.buttonDevide = new System.Windows.Forms.Button();
             this.buttonEqually = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -137,6 +166,7 @@ namespace Kursach
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(204, 22);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -149,7 +179,7 @@ namespace Kursach
             // 
             this.button2.Location = new System.Drawing.Point(101, 118);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(152, 56);
+            this.button2.Size = new System.Drawing.Size(152, 84);
             this.button2.TabIndex = 3;
             this.button2.Text = "Перевести из арабской в римскую";
             this.button2.UseVisualStyleBackColor = true;
@@ -163,6 +193,7 @@ namespace Kursach
             this.buttonI.TabIndex = 4;
             this.buttonI.Text = "I";
             this.buttonI.UseVisualStyleBackColor = true;
+            this.buttonI.Click += new System.EventHandler(this.buttonI_Click);
             // 
             // buttonX
             // 
@@ -172,6 +203,7 @@ namespace Kursach
             this.buttonX.TabIndex = 5;
             this.buttonX.Text = "X";
             this.buttonX.UseVisualStyleBackColor = true;
+            this.buttonX.Click += new System.EventHandler(this.buttonX_Click);
             // 
             // buttonV
             // 
@@ -181,6 +213,7 @@ namespace Kursach
             this.buttonV.TabIndex = 6;
             this.buttonV.Text = "V";
             this.buttonV.UseVisualStyleBackColor = true;
+            this.buttonV.Click += new System.EventHandler(this.buttonV_Click);
             // 
             // buttonL
             // 
@@ -190,6 +223,7 @@ namespace Kursach
             this.buttonL.TabIndex = 7;
             this.buttonL.Text = "L";
             this.buttonL.UseVisualStyleBackColor = true;
+            this.buttonL.Click += new System.EventHandler(this.buttonL_Click);
             // 
             // buttonC
             // 
@@ -199,6 +233,7 @@ namespace Kursach
             this.buttonC.TabIndex = 8;
             this.buttonC.Text = "С";
             this.buttonC.UseVisualStyleBackColor = true;
+            this.buttonC.Click += new System.EventHandler(this.buttonC_Click);
             // 
             // buttonM
             // 
@@ -208,6 +243,7 @@ namespace Kursach
             this.buttonM.TabIndex = 9;
             this.buttonM.Text = "M";
             this.buttonM.UseVisualStyleBackColor = true;
+            this.buttonM.Click += new System.EventHandler(this.buttonM_Click);
             // 
             // buttonPlus
             // 
@@ -217,6 +253,7 @@ namespace Kursach
             this.buttonPlus.TabIndex = 10;
             this.buttonPlus.Text = "+";
             this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
             // 
             // buttonMinus
             // 
@@ -226,6 +263,7 @@ namespace Kursach
             this.buttonMinus.TabIndex = 11;
             this.buttonMinus.Text = "-";
             this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
             // 
             // buttonMulti
             // 
@@ -235,6 +273,7 @@ namespace Kursach
             this.buttonMulti.TabIndex = 12;
             this.buttonMulti.Text = "*";
             this.buttonMulti.UseVisualStyleBackColor = true;
+            this.buttonMulti.Click += new System.EventHandler(this.buttonMulti_Click);
             // 
             // buttonDevide
             // 
@@ -244,6 +283,7 @@ namespace Kursach
             this.buttonDevide.TabIndex = 13;
             this.buttonDevide.Text = "/";
             this.buttonDevide.UseVisualStyleBackColor = true;
+            this.buttonDevide.Click += new System.EventHandler(this.buttonDevide_Click);
             // 
             // buttonEqually
             // 
@@ -253,34 +293,48 @@ namespace Kursach
             this.buttonEqually.TabIndex = 14;
             this.buttonEqually.Text = "=";
             this.buttonEqually.UseVisualStyleBackColor = true;
+            this.buttonEqually.Click += new System.EventHandler(this.buttonEqually_Click);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(547, 373);
+            this.textBox3.Location = new System.Drawing.Point(455, 304);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(204, 22);
+            this.textBox3.Size = new System.Drawing.Size(224, 22);
             this.textBox3.TabIndex = 16;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(547, 308);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(204, 22);
-            this.textBox4.TabIndex = 15;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(570, 288);
+            this.label1.Location = new System.Drawing.Point(650, 307);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(143, 17);
+            this.label1.Size = new System.Drawing.Size(0, 17);
             this.label1.TabIndex = 17;
-            this.label1.Text = "Сюда вводите числа";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(623, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 44);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Сброс результата";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(775, 326);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(178, 44);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Сброс результата";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(570, 353);
+            this.label2.Location = new System.Drawing.Point(503, 236);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(107, 17);
             this.label2.TabIndex = 18;
@@ -292,10 +346,11 @@ namespace Kursach
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(965, 491);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.buttonEqually);
             this.Controls.Add(this.buttonDevide);
             this.Controls.Add(this.buttonMulti);
@@ -336,8 +391,9 @@ namespace Kursach
         private System.Windows.Forms.Button buttonDevide;
         private System.Windows.Forms.Button buttonEqually;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label2;
     }
 }
